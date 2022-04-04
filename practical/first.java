fd1c5957ab66352ffe178149dbf4e77cfb52fbe4
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-04 11:41:04 +0530

    Add files via upload

---
+++ practical/first.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class simple {
+	public static void main(String[] args) {
+		Scanner la = new Scanner(System.in);
+		String name;
+         Integer rollno;
+		System.out.print("Enter your name :");
+		name = la.nextLine();
+		System.out.print("Enter your rollno :");
+		rollno = la.nextInt();
+		System.out.println("Welcome "+name+" Boy "+rollno);
+		System.out.print("Welcome to Java Programming ");
+		System.out.println("Roopak Goutham");
+	}
+}
