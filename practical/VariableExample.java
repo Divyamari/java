a2eb5eac269aff218b1e3da6ac55a0a9bf20e10f
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-05 20:57:57 +0530

    Add files via upload

---
+++ practical/VariableExample.java
@@ -0,0 +1,12 @@
+class VariableExample{
+
+public void display(){
+int a=10,b=20;
+int ans = a+b;
+System.out.println("the addition is:" +ans);
+}
+public static void main (String args[]){
+	VariableExample obj = new VariableExample();
+	obj.display();
+}
+}
