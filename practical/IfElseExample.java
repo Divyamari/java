97a30dccfab652a3baf7a80f25c4219c7ca15f36
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-05 12:01:11 +0530

    Add files via upload

---
+++ practical/IfElseExample.java
@@ -0,0 +1,22 @@
+class IfElseExample{
+	
+public void display() {
+int age = 40;
+
+if(age >=18){ 
+
+System.out.println("you are eligible");
+
+}
+
+else{
+	
+System.out.println("you are not eligible");
+
+}
+}
+public static void main(String args[]){
+IfElseExample obj = new IfElseExample();
+obj.display();
+}
+}
