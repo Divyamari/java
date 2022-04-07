ddb97581ac6e7f761bf88306faca42b4551f1b02
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-07 14:01:36 +0530

    Add files via upload

---
+++ practical/ForloopExample.java
@@ -0,0 +1,13 @@
+class ForloopExample{
+
+public void display() {
+
+for (int i=2; i<=10; i++) {
+System.out.println("value is:" +i);
+}
+}
+public static void main (String args[]) {
+ForloopExample obj = new ForloopExample();
+obj.display();
+}
+}
