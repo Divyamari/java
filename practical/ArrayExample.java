ea28b40fbd183e9d6f6791e279bdb09b3e83cf39
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-08 13:35:44 +0530

    Add files via upload

---
+++ practical/ArrayExample.java
@@ -0,0 +1,14 @@
+class ArrayExample{
+
+public static void main( String args []){
+String [] names = {"Divya", "Mari", "Abi", "Mohan"};
+for (int i=0; i<names.length; i++){
+System.out.println(names[i]);
+}
+System.out.println();
+int [] phone= { 305,136,612,306,907};
+for (int j=0; j<phone.length; j++){
+System.out.println(phone[j]);
+}
+}
+}
