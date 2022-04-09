ac8dd51a0ec63f8bb18afdcfbe9b71964f19ad1f
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-09 19:13:54 +0530

    Add files via upload

---
+++ practical/LogicalOperator.java
@@ -0,0 +1,14 @@
+class LogicalOperator{
+public static void main (String args[]){
+int a,b;
+a=10;
+b=5;
+ 
+ if (a>5 && b<10){
+	System.out.println("true");
+	}
+else {
+	System.out.println("false");
+	}
+}
+}
