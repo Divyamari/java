f0b216253b0483be75c2188c23567a1e36584735
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-11 13:50:59 +0530

    Add files via upload

---
+++ practical/CommandlineExample.java
@@ -0,0 +1,8 @@
+class CommandlineExample{
+public static void main(String args[]){
+	//System.out.println("Hello My Name is "+args[0]);
+	for (int i=0;i<args.length;i++){
+	System.out.println(args[i]);
+	}
+	}
+	}
