d842daebb82d6ccf9a4c62d18d5b676e02c22684
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-22 14:14:39 +0530

    Add files via upload

--- practical/StringExample.java
+++ practical/StringExample.java
@@ -6,13 +6,32 @@ public class StringExample{
 		System.out.println(arr);
 		}
 		public void myfunction(){
-		String m = "divya";
-		String m1 = "divya";
+		String m = "abirami";
+		String m1 = "divya j";
+		String m3= "divya j";
+		String m4 = "mohan";
 		String m2 = new String("divya");
+		System.out.println(m.equals(m1));
+		System.out.println(m.equalsIgnoreCase(m2));
+		System.out.println(m2.equals(m1));
+		System.out.println(m==m2);
+		System.out.println(m3 == m1);
+		System.out.println(m4.compareTo(m));
+		System.out.println(m.compareTo(m4));
+		System.out.println(m1.compareTo(m3));
+		}
+		
+		public void display(){
+		String s = "Anudip" + "Foundation";
+		System.out.println(s);//string s= (new StringBuilder()).append("Anudip").append("Foundation").toString();
+		String s1 = 11+13+ "divya" +20+22;
+		System.out.println(s1);
+		System.out.println(s.concat(s1));
 		}
 		public static void main(String[] args){
 		StringExample obj = new StringExample();
 		obj.show();
 		obj.myfunction();
+		obj.display();
 		}
 		}
