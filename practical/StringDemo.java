4f8df5f5c75c8d012640184be3406a2fd06780be
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-04-25 11:26:41 +0530

    Add files via upload

---
+++ practical/StringDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class StringDemo{
+	public void show(){
+		Scanner s =  new Scanner(System.in);
+		System.out.println("Enter String");
+		String A = s.next();
+		String B = s.next();
+		System.out.println(A.length()+B.length());
+		System.out.println((A.compareTo(B) >0) ? "Yes" : "No");
+		System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+	}
+		public static void main(String args[]){
+		StringDemo d = new StringDemo();
+		d.show();
+		}
+	}
