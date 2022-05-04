16ee755ae588e10b8f6b7fac3e7baa954e5a6db8
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-04 14:05:42 +0530

    Update SequentialExample.java

--- practical/SequentialExample.java
+++ practical/SequentialExample.java
@@ -2,7 +2,7 @@ package threads;
 
 public class SequentialExample {
 
-	public static void main(String[] args) {
+	public void run() {
 		for(int i =1; i<=5; i++) {
 			try {
 				Thread.sleep(800);
@@ -13,4 +13,14 @@ public class SequentialExample {
 			System.out.println(i);
 		}
 	}
+	
+public static void main(String[] args) {
+	SequentialExample t =new SequentialExample();
+	SequentialExample t1 =new SequentialExample();
+	SequentialExample t2 =new SequentialExample();
+	
+	t.run();
+	t1.run();
+	t2.run();
+}
 }
