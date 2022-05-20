c2019cb9e0d2737ccfab4b31640eb8c378250f0f
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-20 13:05:31 +0530

    Add files via upload

---
+++ practical/LambdaExpressionExample.java
@@ -0,0 +1,16 @@
+package adv;
+
+public class LambdaExpressionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FunctionalInterfaceExample d = (a,b)->{
+		//	System.out.println(a+b);
+			return(a+b);
+		//	System.out.println("I am functional interface implemented by Lambda expression.");
+		};
+		System.out.println(d.add(40,50));
+		//d.add();
+	}
+
+}
