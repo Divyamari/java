ffd1cdc4ba21a95466fffd43f91d2c8763c8237a
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-20 13:04:47 +0530

    Update DefaultMethodExample.java

--- practical/DefaultMethodExample.java
+++ practical/DefaultMethodExample.java
@@ -12,5 +12,17 @@ public class DefaultMethodExample implements Sample {
 	public void display(String a) {
 		System.out.println(a);
 	}
-
 }
+
+public interface Sample {
+	void display (String a);//abstract method
+	
+	default void display1() { //default method
+		System.out.println("Iam default method");
+	}
+	static void my() { //static method
+		System.out.println("Iam static method");
+	}
+	
+}
+
