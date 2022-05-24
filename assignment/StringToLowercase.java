810e3127537e21b00819ab6079cfae6a05679adf
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-24 12:28:28 +0530

    Add files via upload

---
+++ assignment/StringToLowercase.java
@@ -0,0 +1,13 @@
+package adv;
+
+public class StringToLowercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "The Quick BroWn FoX!";
+		String new_str = str.toLowerCase();
+		System.out.println("Original String = " + str);
+        System.out.println("String in Lowercase = " + new_str);
+	}
+
+}
