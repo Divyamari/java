2256d69adb7214c139c8e0a09cfd01a8accfcfe1
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-24 12:20:37 +0530

    Add files via upload

---
+++ assignment/ToGetLength.java
@@ -0,0 +1,13 @@
+package adv;
+
+public class ToGetLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "example.com";
+		
+		int length = str.length();
+		System.out.println("The String Length of " + str + " is : " +length);
+	}
+
+}
