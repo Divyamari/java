8daf35b550ff355959e6538fb208d91bdb3a5a91
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-05-24 12:33:24 +0530

    Add files via upload

---
+++ assignment/StringTrim.java
@@ -0,0 +1,13 @@
+package adv;
+
+public class StringTrim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Java Exercise";
+		String new_str= str.trim();
+		System.out.println("Original String = " + str);
+        System.out.println("New String = " + new_str);
+	}
+
+}
