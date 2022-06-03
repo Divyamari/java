00e3fc2ef8605a895a70b64b4febd668dcd9ccf8
Divyamari <102943509+Divyamari@users.noreply.github.com>
2022-06-03 14:09:39 +0530

    Update EmployeeTable.sql

--- MYSQL/EmployeeTable.sql
+++ MYSQL/EmployeeTable.sql
@@ -1,55 +1,40 @@
 use c123;
-
-create table EMP1(
-ID numeric(2),
-Name varchar(10),
-Basic numeric(6,2),
-Designation varchar(10),
-Age numeric(2)
-);
-alter table emp1 modify Basic integer(6);
-alter table emp1 modify Name varchar(15);
-
-create table EMP_trainee(
-Emp_id numeric(2),
-Name varchar(15),
-Basic integer(6),
-Designation varchar(10),
-Age numeric(2)
-);
-insert into emp1 value
-(1,'Rohit',6700,'Manager',24),
-(2,'Sunil',6200,'Engineer',27),
-(3,'Payal',6300,'Engineer',25),
-(4,'Kunal',6700,'Trainee',28),
-(5,'Sunita',6230,'Trainee',26),
-(6,'Bimal',7000,'Trainee',25);
-insert into emp_trainee select*from emp1; 
-select * from emp1,emp_trainee;
-alter table emp1 add Skills varchar(15);
-alter table emp1 add DOJ date;
-
-update emp1 set Skills = 'c' where ID = 1;
-update emp1 set Skills = 'Python' where ID = 2;
-update emp1 set Skills = 'C++' where ID = 3;
-update emp1 set Skills = 'SQL' where ID = 4;
-update emp1 set Skills = 'java' where ID = 5;
-update emp1 set Skills = 'dotnet' where ID = 6;
-
-update emp1 set DOJ = '2022-03-09' where ID = 1;
-update emp1 set DOJ = '2021-12-28' where ID = 2;
-update emp1 set DOJ = '2021-11-05' where ID = 3;
-update emp1 set DOJ = '2022-01-8' where ID = 4;
-update emp1 set DOJ = '2022-02-18' where ID = 5;
-update emp1 set DOJ = '2022-04-23' where ID = 6;
-
-alter table emp_trainee modify Designation varchar(30);
-update emp_trainee set Designation = 'Programmer_Trainee';
-
-alter table emp1 modify ID varchar(5);
-select * from emp1,emp_trainee;
-
-alter table emp1 rename column Age to Age_in_Years;
-delete from emp1 where Designation = 'Trainee';
-
-alter table emp_trainee drop column Age;
+create table EMPLOYEE(
+EMPLOYEE_ID numeric(6),
+LAST_NAME varchar(25),
+JOB_ID varchar(10),
+SALARY numeric(8,2),
+ COMM_PCT numeric(4,2),
+ MGR_ID numeric(6),
+ DEPARTMENT_ID numeric(4)
+ );
+ 
+ insert into employee value
+ (198,'Connell','SH_CLERK',2600,2.5,124,50),
+ (199,'Grant','SH_CLERK',2600,2.5,124,50),
+ (200,'Whalen','AD_ASST',4400,1.3,101,10),
+ (201,'Hartstein','IT_PROG',6000,null,100,20),
+ (202,'Fay','AC_MGR',6500,null,210,20),
+ (203,'Mavris','AD_VP',7500,null,101,40),
+ (204,'Baer','AD_PRES',3500,1.5,101,90),
+ (205,'Higgins','AC_MGR',2300,null,101,60),
+ (206,'Gitz','IT_PROG',5000,null,103,60),
+ (100,'King','AD_ASST',8956,0.3,108,100),
+ (101,'Kochar','SH_CLERK',3400,1.3,118,30);
+ 
+ select last_name, job_id, employee_id from employee;
+ select* from employee where DEPARTMENT_ID = 60;
+ select * from employee where LAST_NAME = 'king';
+ select  job_id from employee;
+ select * from employee where COMM_PCT >=0.1;
+ select * from employee where COMM_PCT;
+ select Employee_id, Department_id , Salary from employee where SALARY >5000;
+ select Last_Name, Salary from employee where SALARY between 4000 and 7000;
+ select * from employee where SALARY =6000 or SALARY=6500 or SALARY=7000;
+ select * from employee where DEPARTMENT_ID=10 or DEPARTMENT_ID=20 or DEPARTMENT_ID=30 or DEPARTMENT_ID=50;
+ select * from employee where SALARY != 5000;
+ select * from employee where JOB_ID = 'SH_CLERK';
+ update employee set JOB_ID = 'Grade_A' where SALARY >= 5000;
+ select * from employee where JOB_ID ='SH_CLERK' or JOB_ID='IT_PROG' or JOB_ID='AD_ASST';
+ select * from employee where JOB_ID ='SH_CLERK' and SALARY<3000;
+select last_name,MGR_ID from employee where SALARY>3000 and MGR_ID=101;
